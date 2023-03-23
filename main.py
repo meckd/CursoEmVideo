8153e76201111e448b30000c754a432a3e420b2f
meckd <borghardt.thiago@gmail.com>
2023-03-23 03:09:02 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,4 @@
+mensagem = input('Digite a mensagem que todo programador deve escrever na sua primeira linha de código: ')
+print('Voce digitou: ', mensagem)
+
+#ALTERADO A PALAVRA COMEÇAR POR ESCREVER
